800e7d886bc348a92cd2456cc6ecfd8d96dbc39e
Ato Araki <ato.araki@gmail.com>
2014-06-24 22:34:39 +0900

    doc: add usage and setting

--- doc.go
+++ doc.go
@@ -1,12 +1,48 @@
 /*
-The gomacs provides editor emacs for golang programing.
+The gomacs provides emacs rapper command for golang programming.
+
+Install
+
+use `go get` command:
+
+   $ go get github.com/atotto/gomacs
+   $ gomacs -gomacs.init        # install emacs lisp from internet.
+   $ gomacs -gomacs.update      # update emacs lisp from internet.
 
 Usage
 
-execute editor:
+Launch:
 
-   $ gomacs
+   $ gomacs                     # launch emacs
 
+The gomacs can use emacs option and operation. for example:
+
+   $ gomacs --help              # show emacs --help
+   $ gomacs main.go             # open main.go
+   $ gomacs +12 main.go         # go to line
+   $ gomacs -rv                 # switch foreground and background color
+       :
+
+Setting
+
+You can use the following `settings.json`:
+
+   {
+       "emacs" : "/usr/local/bin/emacs",
+       "args" : ["-q"]
+   }
+
+The "emacs" defines your emacs execution path.
+You can use any emacs options, use the "args" field. See emacs --help.
+
+The gomacs search the setting file from:
+
+    1 ./.gomacs.json
+    2 ~/.config/gomacs/settings.json
+    3 $GOPATH/src/github.com/atotto/gomacs/settings.json
+
+
+enjoy!
 
 */
 package main
